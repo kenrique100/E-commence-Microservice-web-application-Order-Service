6c8a7581059a1807db520a82301ea1aa6fc4cb18
kenrique100 <ngwakenri2016@gmail.com>
2025-02-17 10:38:13 -0500

    remove redundant entry for isInStock

--- src/main/java/com/akentech/microservices/order/client/InventoryClient.java
+++ src/main/java/com/akentech/microservices/order/client/InventoryClient.java
@@ -3,8 +3,7 @@ package com.akentech.microservices.order.client;
 import com.akentech.microservices.common.dto.InventoryRequest;
 import com.akentech.microservices.common.dto.InventoryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "inventory-service", url = "http://localhost:8087/api/inventory")
 public interface InventoryClient {
