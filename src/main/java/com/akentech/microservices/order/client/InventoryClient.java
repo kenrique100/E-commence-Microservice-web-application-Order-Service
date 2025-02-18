0e38b532124b8fb0a0c72e7c606c63e6acb78274
kenrique100 <ngwakenri2016@gmail.com>
2025-02-18 18:46:21 -0500

    removed openfiegn and replaced wt get exchange

--- src/main/java/com/akentech/microservices/order/client/InventoryClient.java
+++ src/main/java/com/akentech/microservices/order/client/InventoryClient.java
@@ -2,12 +2,11 @@ package com.akentech.microservices.order.client;
 
 import com.akentech.microservices.common.dto.InventoryRequest;
 import com.akentech.microservices.common.dto.InventoryResponse;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8087/api/inventory")
 public interface InventoryClient {
 
-    @PostMapping("/check")
+    @GetExchange("/check")
     InventoryResponse checkInventory(@RequestBody InventoryRequest inventoryRequest);
 }
